fbd3eec17ca02311f67d380da1bf4da8d5aa79f8
= <celotts@gmail.com>
2020-08-06 22:08:34 -0400

    enviroment

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.APP_ENV = process.env.APP_ENV || 'development';
+
+import dotenv = require('dotenv');
+
+dotenv.config({
+    path: `${__dirname}/../config/${process.env.APP_ENV}.env`
+});
+
+console.log("ver---", process.env.APP_FOO);
+
 import express = require('express');
 
 const app: express.Application = express();
